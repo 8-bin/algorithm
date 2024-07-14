62a611b0534bd049fd17149e39616637bf5ef824
Taeho Kim <taeho.kim0218@gmail.com>
2024-07-14 23:02:54 +0900

    [Bronze V] Title: 구구단, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2739. 구구단/구구단.py
+++ 백준/Bronze/2739. 구구단/구구단.py
@@ -2,4 +2,5 @@ num1 = int(input())
 
 for i in range(1, 10) :
     result = num1 * i
-    print(num1, "*" ,i, "=" ,result)
+    # print(num1, "*" ,i, "=" ,result
+    print(f"{num1} * {i} = {result}")
